f6d89326d2b283f72dff728a4e76004b7b1ed7ea
Paul Chobert <paul@chobert.fr>
2021-12-01 08:59:39 -0500

    Pick puzzle as first argument

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
-use std::io::{self, BufRead};
+use std::{
+    env,
+    io::{self, BufRead},
+};
 
 mod d01;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
     let stdin = io::stdin();
     let lines = stdin.lock().lines();
-    let res = d01::count_increases(lines);
-    println!("{}", res);
+    let mut res = None;
+
+    match args.get(1).map(String::as_str).unwrap() {
+        "d01p1" => res = Some(d01::count_increases(lines)),
+        _ => {}
+    }
+    println!("{}", res.unwrap());
 }
