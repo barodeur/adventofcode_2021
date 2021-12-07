3943869d1bd75e10b1b4573f0a9024e004cc1824
Paul Chobert <paul@chobert.fr>
2021-12-07 11:04:48 -0500

    Optimize day 7 puzzle 2 solution

--- ruby/d07p2.rb
+++ ruby/d07p2.rb
@@ -1,13 +1,10 @@
 require_relative "./d07"
 
-$positions = parse($stdin)
 
-def cost(best)
-  $positions.sum { |n|
-    dist = (n - best).abs
-    (dist * (dist + 1)) / 2  
-  }
-end
 
-min, max = $positions.minmax
-puts (min..max).map(&method(:cost)).min
+positions = parse($stdin)
+best = positions.sum / positions.count
+puts positions.sum { |n|
+  dist = (n - best).abs
+  (dist * (dist + 1)) / 2
+}
